a133e51a9f2efbea13e5028b9486c1b8d68bd6d2
Atinder-Pal <atty.pal46@gmail.com>
2020-12-21 13:22:40 -0700

    Added gravity property to force the unicorn downwards on jump

--- unicorn.js
+++ unicorn.js
@@ -1,8 +1,10 @@
 class Unicorn{
     constructor(){
+        this.r = 50;
         this.x = 50;
         this.y = height - 50;
         this.vy = 0; //velocity along y axis, it needs it to jump
+        this.gravity = 0.5;
     }
 
     jump(){
@@ -11,9 +13,11 @@ class Unicorn{
 
     move(){
         this.y +=this.vy;
+        this.vy += this.gravity;
+        this.y = constrain(this.y, 0, height - this.r);
     }
 
     show(){
-        rect(this.x, this.y, 50, 50);
+        rect(this.x, this.y, this.r, this.r);
     }
 }
