11eb4a799f8560717be11000a45442d331e7b908
Atinder-Pal <atty.pal46@gmail.com>
2020-12-21 14:39:14 -0700

    decreased the probability of showing trains;used ml5 soundClassifier to call jump on recognizing 'up' sound

--- sketch.js
+++ sketch.js
@@ -3,8 +3,12 @@ let unicornImage;
 let trainImage;
 let backgroundImage;
 let trains = [];
+let soundClassifier;
 
 function preload(){
+  //Citation https://learn.ml5js.org/#/reference/sound-classifier
+  const options = { probabilityThreshold: 0.95 };
+  soundClassifier = ml5.soundClassifier('SpeechCommands18w', options);
   unicornImage = loadImage('unicorn.png');
   trainImage = loadImage('train.png');
   backgroundImage = loadImage('background.jpg');
@@ -12,6 +16,17 @@ function preload(){
 function setup() {
   createCanvas(800, 500);
   unicorn = new Unicorn();
+  soundClassifier.classify(gotCommand);
+}
+
+function gotCommand(error, results){
+  if(error){
+    console.error(error);
+  }
+  console.log(results[0].label, results[0].confidence);
+  if(results[0].label == 'up'){
+    unicorn.jump();
+  }
 }
 
 function keyPressed(){
@@ -21,7 +36,7 @@ function keyPressed(){
 }
 
 function draw() {
-  if(random(1) < 0.01){
+  if(random(1) < 0.005){
     trains.push(new Train());
   }
 
