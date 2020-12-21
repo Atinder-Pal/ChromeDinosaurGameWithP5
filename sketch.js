7974c4ea25aa6af9aeaeace8dbb108aedb932d20
Atinder-Pal <atty.pal46@gmail.com>
2020-12-21 12:57:12 -0700

    Added code to make Unicorn jump on key press;

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+let unicorn;
+
 function setup() {
   createCanvas(400, 400);
+  unicorn = new Unicorn();
+}
+
+function keyPressed(){
+  if(key == ' '){
+    unicorn.jump();
+  }
 }
 
 function draw() {
   background(220);
+  unicorn.show();
+  unicorn.move();
 }
--- unicorn.js
+++ unicorn.js
@@ -2,6 +2,15 @@ class Unicorn{
     constructor(){
         this.x = 50;
         this.y = height - 50;
+        this.vy = 0; //velocity along y axis, it needs it to jump
+    }
+
+    jump(){
+        this.vy = -5;
+    }
+
+    move(){
+        this.y +=this.vy;
     }
 
     show(){
